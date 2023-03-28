7f759106fbf99fd135bd26f9faf1168248abe9f9
Prem N <43260360+prem-npk@users.noreply.github.com>
2023-03-28 14:20:09 +0530

    code added

---
+++ src/main/java/com/kgisl/springmvcjavaconfig/CustomerController.java
@@ -0,0 +1,59 @@
+package com.kgisl.springmvcjavaconfig;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/")
+     public String showHome() { 
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String listCustomers(Model theModel) {
+        List < Customer > theCustomers = customerService.getCustomers();
+        theModel.addAttribute("customers", theCustomers);
+        return "list-customer";
+    }
+
+    @GetMapping("/showForm")
+    public String showFormForAdd(Model theModel) {
+        Customer theCustomer = new Customer();
+        theModel.addAttribute("customer", theCustomer);
+        return "customer-form";
+    }
+
+    @PostMapping("/saveCustomer")
+    public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+        customerService.saveCustomer(theCustomer);
+        return "redirect:/list";
+    }
+
+    @GetMapping("/updateForm")
+    public String showFormForUpdate(@RequestParam("customerId") int theId,
+        Model theModel) {
+        Customer theCustomer = customerService.getCustomer(theId);
+        theModel.addAttribute("customer", theCustomer);
+        return "customer-form";
+    }
+
+    @GetMapping("/delete")
+    public String deleteCustomer(@RequestParam("customerId") int theId) {
+        customerService.deleteCustomer(theId);
+        return "redirect:/list";
+    }
+}
---
+++ src/main/java/com/kgisl/springmvcjavaconfig/CustomerDAO.java
@@ -0,0 +1,14 @@
+package com.kgisl.springmvcjavaconfig;
+
+import java.util.List;
+
+public interface CustomerDAO {
+
+    public List < Customer > getCustomers();
+
+    public void saveCustomer(Customer theCustomer);
+
+    public Customer getCustomer(int theId);
+
+    public void deleteCustomer(int theId);
+}
